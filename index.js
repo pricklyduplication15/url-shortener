b460541251dfe7d0dad6385b121a58958d4a7752
Yon Wayne <chandler.taylor15@gmail.com>
2024-05-23 21:00:37 +0000

    refactor: internal memory

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
 const app = express();
+const urlparser = require("url");
 
 const port = process.env.PORT || 3000;
 
@@ -22,7 +23,11 @@ let urlDatabase = [];
 app.post("/api/shorturl", function (req, res) {
   console.log(req.body);
   const url = req.body.url;
-  const parsedUrl = urlparser.parse(url);
+
+  // Check if the URL starts with a protocol, if not, prepend 'http://'
+  const fullUrl = /^(?:f|ht)tps?\:\/\//.test(url) ? url : "http://" + url;
+
+  const parsedUrl = urlparser.parse(fullUrl);
 
   // Check if the URL is valid
   dns.lookup(parsedUrl.hostname, async (err, address) => {
@@ -33,34 +38,20 @@ app.post("/api/shorturl", function (req, res) {
       const shortUrl = urlDatabase.length;
 
       // Add URL to the database
-      urlDatabase.push({ url: url, short_url: shortUrl });
+      urlDatabase.push({ url: fullUrl, short_url: shortUrl });
 
       // Send response with original and short URL
-      res.json({ original_url: url, short_url: shortUrl });
+      res.json({ original_url: fullUrl, short_url: shortUrl });
     }
   });
 });
 
-async function generateShortUrl(longUrl, res) {
-  try {
-    // Dynamically import nanoid
-    const { nanoid } = await import("nanoid");
-    const shortId = nanoid(7); // Generate a unique ID with a length of 7
-    urlDatabase[shortId] = longUrl;
-
-    res.json({ original_url: longUrl, short_url: shortId });
-  } catch (error) {
-    console.error("Error generating short URL:", error);
-    res.status(500).send("Error generating short URL");
-  }
-}
-
 app.get("/api/shorturl/:short_url", (req, res) => {
-  const shortId = req.params.id;
-  const longUrl = urlDatabase[shortId];
+  const shortId = parseInt(req.params.short_url); // Parse the short_url parameter as an integer
+  const urlEntry = urlDatabase.find((entry) => entry.short_url === shortId); // Find the entry in the database
 
-  if (longUrl) {
-    res.redirect(longUrl);
+  if (urlEntry) {
+    res.redirect(urlEntry.url); // Redirect to the long URL
   } else {
     res.status(404).send("URL not found");
   }
